d5e125debd2113a8d000c0071587ec1fc805eb1f
“Juliet” <julietasiamah234@gmail.com>
2024-12-17 08:01:54 +0100

    js modificato

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,35 @@
-function Createstars (){
-    const stars = document.querySelector ('.star')
-    
-    
-    
+const stars = document.querySelectorAll ('.star')
+const starsContainer = document.querySelector ('#star-container')
+
+let selectedValue = 0;
+
+function highlightStars (value) {
+    stars.forEach (star => {
+        if (star.dataset.value <= value) {
+            star.classList.add ('selected');
+
+        } else {
+            star.classList.remove ('selected');
+
+        }
+    });
 }
+//evidenzio le stelle
+stars.forEach (star => {
+    star.addEventListener('mouseover',() => {
+        highlightStars (star.dataset.value);
+    })
+})
+
+
+stars.addEventListener('click', () => {
+    selectedValue = stars.dataset.value;
+    highlightStars(selectedValue);
+});
+
+stars.addEventListener('mouseout', () => {
+    highlightStars(selectedValue);
+});
+console.log (stars)
+
+
